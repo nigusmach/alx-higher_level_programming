c86a32455de89dcbd4ae29e13d3338a553f35f0b
nigusmach <nigusmachin@gmail.com>
2022-09-22 11:49:46 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-
 # 4-hidden_discovery.py
 
-# Brennan D Baraban <375@holbertonschool.com>
-
-
-
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-        """Print all names defined by hidden_4 module."""
-
-            import hidden_4
-
-
-
-                names = dir(hidden_4)
-
-                    for name in names:
-
-                                if name[:2] != "__":
-
-                                                print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
