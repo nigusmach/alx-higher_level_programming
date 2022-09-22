7e7fb7e939e7a8cf1f6fed4ef4cc9e6bbdd90f3c
nigusmach <nigusmachin@gmail.com>
2022-09-22 11:51:03 +0100

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,13 +1,8 @@
-n D Baraban <375@holbertonschool.com>
-
-
+#!/usr/bin/python3
+# 5-variable_load.py
 
 if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
 
-        """Print the value of variable a from variable_load_5."""
-
-            from variable_load_5 import a
-
-
-
-                print(a)
+    print(a)
