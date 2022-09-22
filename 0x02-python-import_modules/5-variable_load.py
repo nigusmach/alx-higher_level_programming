70d2d950d6791f01dc175acdf50644ecddbc6b01
nigusmach <nigusmachin@gmail.com>
2022-09-22 03:41:07 -0700

    my first commit

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+n D Baraban <375@holbertonschool.com>
+
+
+
+if __name__ == "__main__":
+
+        """Print the value of variable a from variable_load_5."""
+
+            from variable_load_5 import a
+
+
+
+                print(a)
