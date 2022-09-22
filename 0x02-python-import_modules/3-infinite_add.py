c7082e678a8423741ffdeaede73a86d00c978faf
nigusmach <nigusmachin@gmail.com>
2022-09-22 11:47:33 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-
 # 3-infinite_add.py
 
-# Brennan D Baraban <375@holbertonschool.com>
-
-
-
 if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
 
-        """Print the addition of all arguments."""
-
-            import sys
-
-
-
-                total = 0
-
-                    for i in range(len(sys.argv) - 1):
-
-                                total += int(sys.argv[i + 1])
-
-                                    print("{}".format(total))
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
