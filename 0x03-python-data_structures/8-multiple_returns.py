7c587378c5d5f5b7bf415ae71a372b8f6d964222
nigusmach <nigusmachin@gmail.com>
2022-09-22 22:20:03 -0700

    my first commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,17 @@
+.py
+
+# Brennan D Baraban <375@holbertonschool.com>
+
+
+
+
+
+def multiple_returns(sentence):
+
+        """Returns the length of a string and its first character."""
+
+            if sentence == "":
+
+                        return (0, None)
+
+                        return (len(sentence), sentence[0])
