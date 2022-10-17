e31f5c6ac8134db82e4da8a2f386b7386c95c1c5
nigusmach <nigusmachin@gmail.com>
2022-10-17 01:41:45 -0700

    my first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# 0-rectangle.py
+
+# Brennan D Baraban <375@holbertonschool.com>
+
+"""Defines a Rectangle class."""
+
+
+
+
+
+class Rectangle:
+
+        """Represent a rectangle."""
+
+            pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+more on pyhons and classes project
