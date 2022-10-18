7dcf8d8962b177fedd05f61fc9347e03414959bd
nigusmach <nigusmachin@gmail.com>
2022-10-18 14:08:38 -0700

    commited

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""LockedClass module."""
+
+
+
+
+
+class LockedClass:
+
+        """LockedClass class containing  only __slots__."""
+
+            __slots__ = ['first_name']
