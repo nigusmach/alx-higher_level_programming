b93f0f3a57be06376c089850a8b67abd3c671bc7
nigusmach <nigusmachin@gmail.com>
2022-10-25 20:15:11 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-# 8-load_from_json_file.py
-"""Defines a JSON file-reading function."""
-import json
+""" Module that returns the dictionary description with a simple
+data structure for a JSON serialization of an object
+"""
 
 
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
+def class_to_json(obj):
+    """ Function that retuns the dictionary description of an obj """
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
