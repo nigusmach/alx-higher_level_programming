875f4b34267bf600ccd0138f3fa24c1d6863f803
nigusmach <nigusmachin@gmail.com>
2022-11-30 14:36:39 +0000

    Update 4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,11 +1,6 @@
--- creates the table force_name on your MySQL server.
-
--- force_name description: id INT, name VARCHAR(256) can’t be null
-
+-- creates the table id_not_null on your MySQL server.
+-- id_not_null description: id INT with the default value 1, name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
+-- If the table id_not_null already exists, your script should not fail
 
--- If the table force_name already exists, your script should not fail
-
-
-
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
