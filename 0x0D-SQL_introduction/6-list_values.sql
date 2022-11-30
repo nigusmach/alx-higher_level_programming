77b3d7f9e5f92b0e5ac006eccfae5e0ffc6dcd41
nigusmach <nigusmachin@gmail.com>
2022-11-30 15:16:47 +0000

    Update 6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
 -- All fields should be printed
 -- The database name will be passed as an argument of the mysql command
 
