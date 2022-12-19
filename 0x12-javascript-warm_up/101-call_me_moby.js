7a460165e58b781aaafa6e7f2fed91f62b8c96fe
nigusmach <nigusmachin@gmail.com>
2022-12-18 23:52:18 -0800

    my first commit

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Executes theFunction 'x' times
+//
+// exports.callMeMoby = function (x, theFunction) {
+//
+//   while (x-- > 0) {
+//
+//       theFunction();
+//
+//         }
+//
+//         };
---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+exports.addMeMaybe = function (number, theFunction) {
+
+	  theFunction(number + 1);
+
+};
