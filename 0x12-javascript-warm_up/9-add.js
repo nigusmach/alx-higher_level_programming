ecadd68724cf8fe283ed311ed0f8d138bc951038
nigusmach <nigusmachin@gmail.com>
2022-12-19 07:38:26 +0000

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,21 +1,11 @@
 #!/usr/bin/node
 
-
-
 function add (a, b) {
-
-	  return (a + b);
-
+  return (a + b);
 }
 
-
-
 if (process.argv.length < 4) {
-
-	  console.log('NaN');
-
+  console.log('NaN');
 } else {
-
-	  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
-
+  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
 }
