5653de9eb7bbe52aa52609efae51d0c6d501ee45
nigusmach <nigusmachin@gmail.com>
2022-12-20 15:32:42 +0000

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,23 +1,9 @@
 #!/usr/bin/node
-
-
-
 // Converts a number from base 10 to another base passed as argument
-//
-// // with Closure
-//
-//
-//
-// exports.converter = function (base) {
-//
-//   function myConverter (n) {
-//
-//       return n.toString(base);
-//
-//         }
-//
-//
-//
-//           return myConverter;
-//
-//           };
+// with Closure
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+  return myConverter;
+};
