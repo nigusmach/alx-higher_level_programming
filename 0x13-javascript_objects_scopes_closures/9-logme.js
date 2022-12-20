d1abd2063241f516dfd2b7007814380f7f128514
nigusmach <nigusmachin@gmail.com>
2022-12-20 15:31:44 +0000

    Update 9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
-
-
-
 let counter = 0;
-
-
-
 exports.logMe = function count (item) {
-
-	  console.log(`${counter}: ${item}`);
-
-	  counter += 1;
-
+  console.log(`${counter}: ${item}`);
+  counter += 1;
 };
