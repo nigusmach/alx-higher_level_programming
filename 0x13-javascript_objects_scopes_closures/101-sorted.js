09eedd9a48d349ce623509cb6be2c109bedbe490
nigusmach <nigusmachin@gmail.com>
2022-12-20 20:21:10 +0000

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,41 +1,11 @@
 #!/usr/bin/node
-
 const dict = require('./101-data.js').dict;
-
 let newDict = {};
-
 for (let key in dict) {
-
-	  if (newDict[dict[key]] === undefined) {
-
-		      newDict[dict[key]] = [key];
-
-		    } else {
-
-			        newDict[dict[key]].push(key);
-
-			      }
-
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
 }
-
-console.log(newDict);#!/usr/bin/node
-
-const dict = require('./101-data.js').dict;
-
-let newDict = {};
-
-for (let key in dict) {
-
-	  if (newDict[dict[key]] === undefined) {
-
-		      newDict[dict[key]] = [key];
-
-		    } else {
-
-			        newDict[dict[key]].push(key);
-
-			      }
-
-}
-
 console.log(newDict);
