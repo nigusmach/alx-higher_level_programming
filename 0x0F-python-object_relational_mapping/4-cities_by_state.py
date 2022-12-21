2b81059bb94cefe04adb913eb6613e11df965f53
nigusmach <nigusmachin@gmail.com>
2022-12-21 12:59:02 +0000

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,161 +1,37 @@
 #!/usr/bin/python3
-
 """
-
-This script takes in an argument and
-
-displays all values in the states
-
-where `name` matches the argument
-
-from the database `hbtn_0e_0_usa`.
-
-This time the script is safe from
-
-MySQL injections!
-
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
 """
-
-
 
 import MySQLdb
-
 from sys import argv
 
-
-
 if __name__ == '__main__':
-
-        """
-
-            Access to the database and get the states
-
-                from the database.
-
-                    """
-
-
-
-                        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-
-                                                         passwd=argv[2], db=argv[3])
-
-
-
-                            with db.cursor() as cur:
-
-                                        cur.execute("""
-
-                                                    SELECT
-
-                                                                    *
-
-                                                                                FROM
-
-                                                                                                states
-
-                                                                                                            WHERE
-
-                                                                                                                            name LIKE BINARY %(name)s
-
-                                                                                                                                        ORDER BY
-
-                                                                                                                                                        states.id ASC
-
-                                                                                                                                                                """, {
-
-                                                                                                                                                                                'name': argv[4]
-
-                                                                                                                                                                                        })
-
-
-
-                                                                                                                                                                        rows = cur.fetchall()
-
-
-
-                                                                                                                                                                            if rows is not None:
-
-                                                                                                                                                                                        for row in rows:
-
-                                                                                                                                                                                                        print(row)#!/usr/bin/python3
-
-                                                                                                                                                                                                        """
-
-                                                                                                                                                                                                        This script takes in an argument and
-
-                                                                                                                                                                                                        displays all values in the states
-
-                                                                                                                                                                                                        where `name` matches the argument
-
-                                                                                                                                                                                                        from the database `hbtn_0e_0_usa`.
-
-                                                                                                                                                                                                        This time the script is safe from
-
-                                                                                                                                                                                                        MySQL injections!
-
-                                                                                                                                                                                                        """
-
-
-
-                                                                                                                                                                                                        import MySQLdb
-
-                                                                                                                                                                                                        from sys import argv
-
-
-
-                                                                                                                                                                                                        if __name__ == '__main__':
-
-                                                                                                                                                                                                                """
-
-                                                                                                                                                                                                                    Access to the database and get the states
-
-                                                                                                                                                                                                                        from the database.
-
-                                                                                                                                                                                                                            """
-
-
-
-                                                                                                                                                                                                                                db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-
-                                                                                                                                                                                                                                                                 passwd=argv[2], db=argv[3])
-
-
-
-                                                                                                                                                                                                                                    with db.cursor() as cur:
-
-                                                                                                                                                                                                                                                cur.execute("""
-
-                                                                                                                                                                                                                                                            SELECT
-
-                                                                                                                                                                                                                                                                            *
-
-                                                                                                                                                                                                                                                                                        FROM
-
-                                                                                                                                                                                                                                                                                                        states
-
-                                                                                                                                                                                                                                                                                                                    WHERE
-
-                                                                                                                                                                                                                                                                                                                                    name LIKE BINARY %(name)s
-
-                                                                                                                                                                                                                                                                                                                                                ORDER BY
-
-                                                                                                                                                                                                                                                                                                                                                                states.id ASC
-
-                                                                                                                                                                                                                                                                                                                                                                        """, {
-
-                                                                                                                                                                                                                                                                                                                                                                                        'name': argv[4]
-
-                                                                                                                                                                                                                                                                                                                                                                                                })
-
-
-
-                                                                                                                                                                                                                                                                                                                                                                                rows = cur.fetchall()
-
-
-
-                                                                                                                                                                                                                                                                                                                                                                                    if rows is not None:
-
-                                                                                                                                                                                                                                                                                                                                                                                                for row in rows:
-
-                                                                                                                                                                                                                                                                                                                                                                                                                print(row)
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
