39c9a20fc1c8b4fc6656d01285345b55904b7769
nigusmach <nigusmachin@gmail.com>
2023-01-16 18:18:09 +0000

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
- in a URL and displays all HTTP methods the server will accept
-
- curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
+#!/bin/bash
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
