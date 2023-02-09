fb611edf7a2261963fe65dbdb30c700ccce8c818
nigusmach <nigusmachin@gmail.com>
2023-02-09 11:49:53 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,9 @@
-t request = require('request');
+#!/usr/bin/node
 
+const request = require('request');
 const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-
-
 request(starWarsUri, function (_err, _res, body) {
-
-	  body = JSON.parse(body);
-
-	  console.log(body.title);
-
+  body = JSON.parse(body);
+  console.log(body.title);
 });
